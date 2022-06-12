ed6760418b352d93a184d1d114f7963bf85f5293
Ujjwol Dandekhya <ujjwol.dandekhya@ssh.com>
2022-06-12 18:47:49 +0300

    Changed datastructure of Games from List to Dict and relevant changes in code to reflect the changes

--- Repository/GameRepository.py
+++ Repository/GameRepository.py
@@ -1,51 +1,53 @@
-from Model.Game import Game
-
 class GameRepository:
     def __init__(self):
-        self.games = [
-            {
+        '''dictionary which holds the state of all games'''
+        self.games = {
+            "1": {
                 "game_id": "1",
                 "board": "X--------",
                 "status": "RUNNING",
                 "player": "O",
                 "computer": "X"
             },
-            {
+            "2" : {
                 "game_id": "2",
                 "board": "O--O---XX",
                 "status": "RUNNING",
                 "player": "O",
                 "computer": "X"
             },
-            {
+            "3": {
                 "game_id": "3",
                 "board": "X-----O--",
                 "status": "RUNNING",
                 "player": "X",
                 "computer": "O"
             }
-        ]
+        }
     
     def get_games(self):
         return self.games
 
     def get_game(self, game_id):
-        # logic
-        for game in self.games:
-            if game['game_id'] == game_id:
-                return game
-        #abort(404, message="Game {} doesn't exist".format(game_id))
+        if game_id in self.games.keys():
+            return self.games[game_id]
         return None
         
     def add_game(self, game):
-        self.games.append(game.toJSON())
+        self.games[game.game_id] = game.toJSON()
 
     def delete_game(self, game_id):
-        game = self.get_game(game_id)
-        self.games.remove(game)
-        return True
+        if game_id in self.games.keys():
+            del self.games[game_id]
+            return True
+        else: 
+            return False
 
-    #change datastructure to dictionary for easier update
     def update_game(self, game_id, game):
-        self.games = [game.toJSON() if ele['game_id']== game_id else ele for ele in self.games]
-        print(self.games)
+        if game_id in self.games.keys():
+            self.games[game_id] = game.toJSON()
+            print(self.games)
+            return True
+        else:
+            return False
+        
